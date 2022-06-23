f8df7db2e56b0fcafbe0ff90bc4e492a3030d663
oibioib <pavelbodal@gmail.com>
2022-06-23 23:03:42 +0300

    feat: write solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,6 +4,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Create transformed array based on the control sequences that original
  * array contains
  * 
+ * --discard-next (excludes the next element of the array from the transformed array)
+ * --discard-prev (excludes the previous element of the array from the transformed array)
+ * --double-next (duplicates the next element of the array in the transformed array)
+ * --double-prev (duplicates the previous element of the array in the transformed array.)
+ * 
  * @param {Array} arr initial array
  * @returns {Array} transformed array
  * 
@@ -13,9 +18,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform( /* arr */ ) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error("'arr' parameter must be an instance of the Array!");
+    }
+    
+    const arrCopy = [...arr];
+    const result = [];
+
+    arrCopy.forEach((element, i) => {
+        switch (element) {
+            case '--discard-next':
+                arrCopy[i + 1] ? arrCopy.splice(i, 2, null) : null;
+                break;
+            case '--discard-prev':
+                arrCopy[i - 1] ? result.pop() : null;
+                break;
+            case '--double-next':
+                arrCopy[i + 1] ? result.push(arrCopy[i + 1]) : null;
+                break;
+            case '--double-prev':
+                arrCopy[i - 1] ? result.push(arrCopy[i - 1]) : null;
+                break;
+            default:
+                result.push(element);
+        }
+    });
+    return result;
 }
 
 module.exports = {
